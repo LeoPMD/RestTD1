e5e83d8ab4d8579441b4e0dbff1037e1ac51e7bc
LeoPMD <leo.pellegrino2b@gmail.com>
2022-05-30 12:15:36 +0200

    Ajout de la fonction getAllStudents

--- src/main/java/com/inti/RestTD1/controller/EtudantController.java
+++ src/main/java/com/inti/RestTD1/controller/EtudantController.java
@@ -1,10 +1,26 @@
 package com.inti.RestTD1.controller;
 
+import java.util.List;
+
+import com.inti.RestTD1.model.Etudiant;
+import com.inti.RestTD1.repository.EtudiantRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EtudantController {
 	
+	@Autowired
+	EtudiantRepository etudiantRepository;
 	
+	@GetMapping("/students")
+	public ResponseEntity<List<Etudiant>> getAllStudents()
+	{
+		return new ResponseEntity<List<Etudiant>>(etudiantRepository.findAll(), HttpStatus.OK);
+	}
 
 }
